6f04d41624b4e5edb0e09f6497bf9d576e97f3b0
Matt Dunn <contact@mattonsoftware.com>
2015-07-01 14:04:43 +1000

    refactoring first rule to use empty collection

--- src/test/java/org/gol/UnderPopulationRuleTest.java
+++ src/test/java/org/gol/UnderPopulationRuleTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 import org.gol.rules.Action;
 import org.gol.rules.IRule;
@@ -13,17 +14,22 @@ import org.junit.Test;
 public class UnderPopulationRuleTest {
 
 	@Test
-	public void givenAliveCellWithLessThanTwoNeighboursCellShouldStillBeDead() {
+	public void givenAliveCellWithLessThanTwoNeighboursCellShouldDie() {
 		IRule rule = new UnderPopulationRule();
 		assertEquals(rule.whatNext(Cell.ALIVE(), this.generateNeighboursWithZeroAliveCells()), Action.DIE);
 		assertEquals(rule.whatNext(Cell.ALIVE(), this.generateNeighboursWithOneAliveCell()), Action.DIE);
 	}
 	
 	@Test
-	public void givenDeadCellWithLessThanTwoNeighboursCellShouldStillBeDead() {
+	public void givenAliveCellWithTwoOrMoreNeighboursNothingShouldHappenToCell() {
 		IRule rule = new UnderPopulationRule();
-		assertEquals(rule.whatNext(Cell.DEAD(), this.generateNeighboursWithZeroAliveCells()), Action.NOTHING);
-		assertEquals(rule.whatNext(Cell.DEAD(), this.generateNeighboursWithOneAliveCell()), Action.NOTHING);
+		assertEquals(rule.whatNext(Cell.ALIVE(), this.generateNeighboursWithTwoAliveCells()), Action.NOTHING);
+	}
+	
+	@Test
+	public void givenDeadCellNothingShouldHappenToCell() {
+		IRule rule = new UnderPopulationRule();
+		assertEquals(rule.whatNext(Cell.DEAD(), Collections.emptyList()), Action.NOTHING);
 	}
 
 	private Collection<Cell> generateNeighboursWithZeroAliveCells() {
@@ -32,6 +38,10 @@ public class UnderPopulationRuleTest {
 	
 	private Collection<Cell> generateNeighboursWithOneAliveCell() {
 		return generateNeighboursWithNumberOfAliveCell(1);
+	}
+	
+	private Collection<Cell> generateNeighboursWithTwoAliveCells() {
+		return generateNeighboursWithNumberOfAliveCell(2);
 	}
 	
 	private Collection<Cell> generateNeighboursWithNumberOfAliveCell(int aliveCells) {
