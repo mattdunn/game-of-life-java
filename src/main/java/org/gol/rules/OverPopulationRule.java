db58c08404369493d4ee29673193e53ae13df722
Matt Dunn <contact@mattonsoftware.com>
2015-07-15 18:00:08 +1000

    implement over population rule

--- src/main/java/org/gol/rules/OverPopulationRule.java
+++ src/main/java/org/gol/rules/OverPopulationRule.java
@@ -1,15 +1,22 @@
 package org.gol.rules;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 import org.gol.Cell;
 
 public class OverPopulationRule implements IRule {
 
 	@Override
-	public Action whatNext(Cell cell, Collection<Cell> neighbours) {
-		// TODO Auto-generated method stub
-		return null;
+	public Action whatNext(Cell cell, Collection<Cell> neighbours) {		
+		if(cell.isDead()){
+			return  Action.NOTHING;
+		}
+		Long countOfAliveNeighbours = neighbours.stream().filter((c) -> c.isAlive()).collect(Collectors.counting());	
+		if (countOfAliveNeighbours > 3){
+			return Action.DIE;
+		}
+		return Action.NOTHING;
 	}
 
 }
--- src/test/java/org/gol/OverPopulationRuleTest.java
+++ src/test/java/org/gol/OverPopulationRuleTest.java
@@ -1,21 +1,43 @@
 package org.gol;
 
+import static org.fest.assertions.Assertions.assertThat;
 import static org.gol.util.NeighboursGenerator.generateNeighboursWithNumberOfAliveCell;
-import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.gol.rules.Action;
 import org.gol.rules.IRule;
 import org.gol.rules.OverPopulationRule;
 import org.junit.Test;
 
-
 public class OverPopulationRuleTest {
 
 	private IRule rule = new OverPopulationRule();
+
+	@Test
+	public void givenAliveCellWithFourAliveNeighboursShouldDie() {
+		Collection<Cell> neighbours = generateNeighboursWithNumberOfAliveCell(4);
+
+		Action result = rule.whatNext(Cell.ALIVE(), neighbours);
+
+		assertThat(result).isEqualTo(Action.DIE);
+	}
+
+	@Test
+	public void givenAliveCellWithThreeAliveNeighboursShouldDoNothing() {
+		Collection<Cell> threeNeighbours = generateNeighboursWithNumberOfAliveCell(3);
+
+		Action result = rule.whatNext(Cell.ALIVE(), threeNeighbours);
+
+		assertThat(result).isEqualTo(Action.NOTHING);
+	}
 	
 	@Test
-	public void givenAliveCellWithFourNeighboursShouldDie()
-	{
-		assertEquals(rule.whatNext(Cell.ALIVE(), generateNeighboursWithNumberOfAliveCell(4)), Action.DIE);
+	public void givenDeadCellShouldDoNothing() {
+		Action result = rule.whatNext(Cell.DEAD(), null);
+
+		assertThat(result).isEqualTo(Action.NOTHING);
 	}
- }
+	
+}
